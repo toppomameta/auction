26939c65fea9ca9ac642a480d194b509d4a97954
toppomameta <toppo850122@gmail.com>
2018-03-10 11:09:55 +0900

    詳細と一覧表作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/items' => 'items#index'
+  get '/items/:id' => 'items#show'
 end
